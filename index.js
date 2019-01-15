0eed85f3aa2e4de1529a9d368abe260ae063b942
daniel garcía ramírez <karenroolk@gmail.com>
2019-01-14 21:22:31 -0600

    Se aagrego la funcionalidad

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-function agregrTarea(){
-    var lista = document.getElementsByClassName("todoList");
-    var texto = document.getElementById("tareaAdd").value;
-    lista[0].innerHTML = ('<div><p>'+texto+'</p><input type="button" onclick="delete(this)"></div>');
-}
+ document.addEventListener("DOMContentLoaded", function(event) {
+    const $lista = document.getElementById("todoList");
+    const $texto = document.getElementById("textItem");
+    var arrayTareas = [];
+    document.getElementById("addItem").addEventListener("click", agregrTarea);
+    
+    function agregrTarea(){
+        var item = document.createElement('li');
+        var textItem = document.createTextNode($texto.value);
+        var deleteButton = document.createElement('button');
+        deleteButton.addEventListener("click", deleteItem);
+        item.append(textItem);
+        item.append(deleteButton);        
+        $lista.appendChild(item);
+    }
+
+    function deleteItem(){
+        console.log("Se eliminoo elemento", this.parentElement);
+        $lista.removeChild(this.parentElement);
+    }
+
+  });
+
