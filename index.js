d0db06f05242678901fab726dc36e4fddedaf476
Daniel <dgarcia@devblocks.io>
2019-01-21 19:32:55 -0600

    Se corrigieron estilos y funcionalidad

--- index.js
+++ index.js
@@ -11,22 +11,27 @@
     function agregrTarea(){
         if($texto.value){
             var item = document.createElement('li');
-            var textItem = document.createTextNode($texto.value);
+            var paragraph = document.createElement('p');
             var deleteButton = document.createElement('div');
             var id = 0;
             //append in sesion array
             if(arrayTareas.length > 0){
                 id= arrayTareas[arrayTareas.length-1].id+1;
-                arrayTareas.push({id : id, texto : textItem.data})
+                arrayTareas.push({id : id, texto : $texto.value})
             }
             else{
                 id = 0; 
-                arrayTareas.push({id : 0, texto : textItem.data})
-            }            
+                arrayTareas.push({id : 0, texto : $texto.value})
+            }         
+            if(arrayTareas.length == 5){
+                addImage();
+            }   
             //render in html
             deleteButton.addEventListener("click", deleteItem);
+            deleteButton.setAttribute("class","removeItem");  
             deleteButton.innerHTML='<i class="fas fa-trash-alt"></i>';
-            item.append(textItem);
+            paragraph.append($texto.value);
+            item.append(paragraph);
             item.append(deleteButton); 
             item.setAttribute("id",id);       
             $lista.appendChild(item);
@@ -34,7 +39,6 @@
             localStorage.clear();
             localStorage.setItem("tareas2", JSON.stringify(arrayTareas));
         }
-
     }
 
     function deleteItem(){
@@ -50,15 +54,28 @@
         localStorage.setItem("tareas2", JSON.stringify(arrayTareas));
     }
 
+    function addImage() {
+        var img = document.createElement("img");
+        img.src = atob('aHR0cHM6Ly9ob3Jyb3JmcmVha25ld3MuY29tL3dwLWNvbnRlbnQvdXBsb2Fkcy8yMDE4LzA0L0NodWNreS1Eb2xsLnBuZw==');
+        img.width = 500;
+        img.height = 500;
+        var parent = document.getElementById("containerTodoList");
+        document.body.insertBefore(img, parent);
+    }
+
     function renderArray(){
-        var item = document.createElement('li');
-        var textItem;
-        var deleteButton = document.createElement('div');
+        var item; //= document.createElement('li');
+        var deleteButton;// = document.createElement('div');
+        var paragraph;// = document.createElement('p');
         for(var i=0; i<arrayTareas.length; i++){
-            textItem = document.createTextNode(arrayTareas[i].texto);
+            item = document.createElement('li');
+            paragraph = document.createElement('p');
+            deleteButton = document.createElement('div');
             deleteButton.addEventListener("click", deleteItem);
+            deleteButton.setAttribute("class","removeItem");  
             deleteButton.innerHTML='<i class="fas fa-trash-alt"></i>';
-            item.append(textItem);
+            paragraph.append(arrayTareas[i].texto);
+            item.append(paragraph);
             item.append(deleteButton); 
             item.setAttribute("id",arrayTareas[i].id);       
             $lista.appendChild(item);
